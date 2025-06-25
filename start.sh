fa717eaeba39fc305b61afb8f87d9f5523202983
Serendipity <serendipity@yuanshanghanshans-MacBook-Air.local>
2025-06-24 20:52:15 -0400

    Updated so it auto links fzf and handle macOS as well.

--- start.sh
+++ start.sh
@@ -9,7 +9,17 @@ THEME_DIR="$HOME/.vim/pack/themes/start"
 mkdir -p "$PLUG_DIR"
 mkdir -p "$THEME_DIR"
 
-echo "Installing fzf binary..."
+echo "---Installing system dependencies---"
+if [[ "$(uname)" == "Darwin" ]]; then
+  echo "Assuming OS is mac"
+  sudo apt update
+  sudo apt install -y ripgrep vim git curl
+else
+  echo "Assuming OS is linux"
+  brew install -y ripgrep vim git curl
+fi
+
+echo "---Installing fzf binary---"
 if [ ! -d "$HOME/.fzf" ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install --no-key-bindings --no-completion --no-update-rc
@@ -17,17 +27,16 @@ else
   echo "fzf already installed"
 fi
 
-echo "Installing fzf.vim..."
+echo "---Installing fzf.vim---"
 git clone --depth 1 https://github.com/junegunn/fzf.vim.git "$PLUG_DIR/fzf.vim"
 
-echo "Installing wiki.vim..."
+echo "Linking fzf Vim plugin files..."
+ln -sf ~/.fzf/plugin "$PLUG_DIR/fzf"
+ln -sf ~/.fzf/autoload "$PLUG_DIR/fzf"
+
+echo "---Installing wiki.vim---"
 git clone --depth 1 https://github.com/lervag/wiki.vim.git "$PLUG_DIR/wiki.vim"
 
-echo "Installing nightfly colorscheme..."
+echo "---Installing nightfly colorscheme---"
 git clone --depth 1 https://github.com/bluz71/vim-nightfly-colors.git "$THEME_DIR/nightfly"
 
-echo "Installing system dependencies..."
-
-# Install whatever needed
-sudo apt update
-sudo apt install -y ripgrep vim git curl
